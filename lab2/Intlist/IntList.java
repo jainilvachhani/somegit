207fb8d0b2041dfa0b4e3734a1a09805f2fc3017
Jainil Vachhani <jainilvachhani@gmail.com>
2019-01-20 17:09:59 +0530

    Add files via upload

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -104,6 +104,12 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
+        if(A==null){
+            return B;
+        }
+        if(B==null){
+            return A;
+        }
         IntList res = new IntList(A.first , null);
         IntList ptr = res;
         A = A.rest;
