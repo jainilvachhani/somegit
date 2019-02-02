4ca3c018828bb75b35f36304d12fa22eedf7e477
Jainil Vachhani <jainilvachhani@gmail.com>
2019-02-03 02:45:52 +0530

    Add files via upload

---
+++ hw2/hw2/Percolation.java
@@ -0,0 +1,79 @@
+package hw2;
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+
+    boolean[][] grid;
+    private int top;
+    private int bottom;
+    int totalOpenSites;
+    WeightedQuickUnionUF weightedQuickUnionUF;
+
+    private int xyTo1D(int r, int c, int n){
+        return  r*n + c;
+    }
+
+    public Percolation(int n){
+
+        if(n<=0){
+            throw new IllegalArgumentException();
+        }
+        grid = new boolean[n][n];
+        int i,j;
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                grid[i][j] = false;
+            }
+        }
+        weightedQuickUnionUF = new WeightedQuickUnionUF(n*n + 2);
+        top = n*n;
+        bottom = n*n+1;
+        totalOpenSites = 0;
+    }
+
+    public void open(int r, int c){
+        grid[r][c] = true;
+        if(r-1>=0 && grid[r-1][c]){
+            weightedQuickUnionUF.union(xyTo1D(r-1,c,grid.length),xyTo1D(r,c,grid.length));
+        }
+        if(r+1<grid.length && grid[r+1][c]){
+            weightedQuickUnionUF.union(xyTo1D(r+1,c,grid.length),xyTo1D(r,c,grid.length));
+        }
+        if(c-1>=0 && grid[r][c-1]){
+
+            weightedQuickUnionUF.union(xyTo1D(r,c-1,grid.length),xyTo1D(r,c,grid.length));
+        }
+        if(c+1<grid.length && grid[r][c+1]){
+
+            weightedQuickUnionUF.union(xyTo1D(r,c+1,grid.length),xyTo1D(r,c,grid.length));
+        }
+        if(r==grid.length-1){
+            weightedQuickUnionUF.union(xyTo1D(r,c,grid.length),top);
+        }
+        if(r==0){
+            weightedQuickUnionUF.union(xyTo1D(r,c,grid.length),bottom);
+        }
+        totalOpenSites++;
+    }
+
+    public boolean isOpen(int r, int c){
+        return grid[r][c];
+    }
+
+    public boolean isFull(int r, int c){
+        return weightedQuickUnionUF.connected(xyTo1D(r,c,grid.length),bottom);
+    }
+
+    public int numberOfOpenSites(){
+        return totalOpenSites;
+    }
+
+    public boolean percolates(){
+        return weightedQuickUnionUF.connected(top,bottom);
+    }
+
+    public static void main(String args[]){
+    }
+
+}
---
+++ hw2/hw2/PercolationStats.java
@@ -0,0 +1,50 @@
+package hw2;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+
+public class PercolationStats {
+
+    public double cnt;
+    public double mean;
+    public double stdDev;
+    public double confidenceLow;
+    public double confidenceHigh;
+    int[] numberOfOpenSites;
+
+    public PercolationStats(int N, int T, PercolationFactory pf){
+        if(N<=0 || T<=0){
+            throw new IllegalArgumentException();
+        }
+        int i;
+        numberOfOpenSites = new int[T];
+        for(i=0;i<T;i++){
+            Percolation percolation = pf.make(N);
+            while (!percolation.percolates()){
+                percolation.open(StdRandom.uniform(N),StdRandom.uniform(N));
+            }
+            cnt += percolation.numberOfOpenSites();
+            numberOfOpenSites[i] = percolation.numberOfOpenSites();
+        }
+        mean = StdStats.mean(numberOfOpenSites);
+        stdDev = StdStats.stddev(numberOfOpenSites);
+        confidenceLow = mean - (1.96* stdDev)/(Math.sqrt(T));
+        confidenceHigh = mean + (1.96* stdDev)/(Math.sqrt(T));
+    }
+
+    public double mean(){
+        return mean;
+    }
+
+    public double stddev(){
+        return stdDev;
+    }
+
+    public double confidenceLow(){
+        return confidenceLow;
+    }
+
+    public double confidenceHigh(){
+        return confidenceHigh;
+    }
+}
